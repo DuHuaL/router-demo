b00a7956dbbb48a1ab085c694756c55594b71880
DuHuaL <469580396@qq.com>
2020-09-07 12:43:13 +0800

    组件的搭建及放置位置

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
-    <router-view/>
+    <app-header></app-header>
+    <div class="container-fluid">
+      <div class="row">
+        <app-sidebar></app-sidebar>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <router-view/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import appHeader from '@/components/app-header';
+import appSidebar from '@/components/app-sidebar';
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    appHeader,
+    appSidebar
+  }
 };
 </script>
 
---
+++ src/components/app-sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-sm-3 col-md-2 sidebar">
+    <ul class="nav nav-sidebar">
+      <li class="active">
+        <a href="#">正在热播</a>
+      </li>
+      <li>
+        <a href="#">即将上映</a>
+      </li>
+      <li>
+        <a href="#">Top250</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
