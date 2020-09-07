781a73011281ad4ab4d42d6ec91be9ef0b211b62
DuHuaL <469580396@qq.com>
2020-09-07 16:22:28 +0800

    配置路由

--- src/components/app-sidebar.vue
+++ src/components/app-sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-      <li class="active">
+      <!-- <li class="active">
         <a href="#">正在热播</a>
       </li>
       <li>
@@ -9,7 +9,18 @@
       </li>
       <li>
         <a href="#">Top250</a>
-      </li>
+      </li> -->
+      <!-- tag属性是指外层包裹的标签 -->
+      <!-- 高亮显示 -->
+      <router-link active-class="active" :to="{ name: 'in_theaters' }" tag="li">
+        <a href="#">正在热播</a>
+      </router-link>
+      <router-link active-class="active" :to="{ name: 'coming_soon'}" tag="li">
+        <a href="#">即将上映</a>
+      </router-link>
+      <router-link active-class="active" :to="{ name: 'top250' }" tag="li">
+        <a href="#">Top50</a>
+      </router-link>
     </ul>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,10 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
-    { name: 'movieList', path: '/', component: movieList }
+    // 由于展示的内容样式是一样的，只是数据不同，这里使用同一个组件，实现路由跳转
+    { name: 'home', path: '/', redirect: { name: 'in_theaters' } },
+    { name: 'in_theaters', path: '/in_theaters', component: movieList },
+    { name: 'coming_soon', path: '/comming_soon', component: movieList },
+    { name: 'top250', path: '/top250', component: movieList }
   ]
 });
